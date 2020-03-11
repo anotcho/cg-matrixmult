5a782c47ea2d39126c2787b7c61a681d8b74646e
anotcho <55111098+anotcho@users.noreply.github.com>
2020-03-10 22:41:21 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,7 @@ function CalculateTransformedVertex(vertex) {
 function ChangeTransform(index, type, values) {
     app.transforms[index].type = type;
     // update `app.transforms[index].mat4x4`
+    Mat4x4Translate(mat4x4, values);
 
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
